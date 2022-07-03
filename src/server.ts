fc5b44e7dd301b97637dd3f51acfd45d1cfa8530
Thomaz Takashi Oda Toyama <tttoyama.fcamara@rd.com.br>
2022-07-03 02:47:34 -0300

    chore: name

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,7 @@
 import express, { Response } from 'express';
 
 const server = express();
-
 server.get('/', (request, response: Response) => {
   return response.send('Hello!');
 });
-
 export default server;
